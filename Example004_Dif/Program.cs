a57cff3f29d1f6ea1b5bd03bf0d28c8267cec10b
Aleks37 <lao35180972643335589007>
2023-08-08 21:51:49 +0400

    !

--- Example004_Dif/Program.cs
+++ Example004_Dif/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numberA = 54;
+double numberB = 20;
+Console.WriteLine(numberA / numberB);
