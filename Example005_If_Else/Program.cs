963024258b58c8ba289a35f05468581f35cd4aa0
Aleks37 <lao35180972643335589007>
2023-08-08 22:10:01 +0400

    add example005_if_else

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "лена")
+{
+    Console.WriteLine("Ураа! Неужели это рыжик?)))");
+}
+else
+{
+    Console.Write("Привет, "); 
+    Console.WriteLine(username); 
+}
