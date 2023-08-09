bd21c4c9b967e94e6ac45c9f13c6ce9fd656f761
Aleks37 <lao35180972643335589007>
2023-08-09 22:33:59 +0400

    Commit 3 Add Task2

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,3 +1,8 @@
-﻿string input = Console.ReadLine();
-int number = Convert.ToInt32(input);
+﻿//string input = Console.ReadLine();
+//int number = Convert.ToInt32(input);
 
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = number * number;
+Console.WriteLine("Наш результат = " + result);
