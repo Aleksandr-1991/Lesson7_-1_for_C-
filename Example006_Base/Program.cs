10a95af13d14590c0c804a0ed7bfbaa0bd0346a2
Aleks37 <lao35180972643335589007>
2023-08-09 19:14:31 +0400

    add example006_base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int f = 5;
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (f > max) max = f;
+Console.WriteLine("max = ");
+Console.WriteLine(max);
