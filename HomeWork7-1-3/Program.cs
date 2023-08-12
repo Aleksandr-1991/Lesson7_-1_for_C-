a9dd36c314c597cd21d5d9f3f13ed7d2f121a28d
Aleks37 <lao35180972643335589007>
2023-08-12 21:02:05 +0400

    Add HomeWork 7-1-3

---
+++ HomeWork7-1-3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result_division = number % 2;
+if (result_division == 1)
+{
+    Console.WriteLine("Число является не чётным!");
+}
+else 
+{
+    Console.WriteLine("Число является чётным!");
+};
