cba011b749e485d8afb3165504bfdaf94b4757ff
Aleks37 <lao35180972643335589007>
2023-09-04 20:56:30 +0400

    Коммит после переименования

--- HomeWork7-1-4/Program.cs
+++ HomeWork7-1-4/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("Введите натуральное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int i = 1;
-while (i < (number + 1))
+while (i <= number)
 {
     int result_division = i % 2;
     if (result_division == 0)
