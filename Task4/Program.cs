9aeab560af8cf6767c7caf5dd57becb133e6670e
Aleks37 <lao35180972643335589007>
2023-11-11 18:11:17 +0400

    Last commit from 11.11.23

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int i = 0 - number;
+int i = - number;
 while (i < (number + 1))
 {
     Console.Write(i + ", ");
